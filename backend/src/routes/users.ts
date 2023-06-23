c8ec526f4b32a3af66c593bc6757ab48a7649ed2
lauri-ra <lrautiala+1@gmail.com>
2023-06-23 21:05:59 +0300

    Updated users router

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,12 +1,19 @@
 import express from 'express';
 import db from '../db/connect';
 import { users, User } from '../db/schema';
+import { eq } from 'drizzle-orm';
 
 const router = express.Router();
 
 router.get('/', async (_request, response) => {
 	const allUsers: User[] = await db.select().from(users);
 	response.json(allUsers);
+});
+
+router.get('/:id', async (request, response) => {
+	const id = Number(request.params.id);
+	const user: User[] = await db.select().from(users).where(eq(users.id, id));
+	response.json(user);
 });
 
 export default router;
