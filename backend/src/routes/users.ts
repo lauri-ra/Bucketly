c219f5673e2cc288a895ac7aa499ab9135059c75
lauri-ra <lrautiala+1@gmail.com>
2023-07-15 00:07:01 +0300

    post new user route added

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,7 +1,8 @@
 import express from 'express';
+import bcrypt from 'bcrypt';
 import db from '../db/connect';
 import { users, User } from '../db/schema';
-import { eq } from 'drizzle-orm';
+import { eq, or } from 'drizzle-orm';
 
 const router = express.Router();
 
@@ -14,6 +15,37 @@ router.get('/:id', async (request, response) => {
 	const id = Number(request.params.id);
 	const user: User[] = await db.select().from(users).where(eq(users.id, id));
 	response.json(user);
+});
+
+router.post('/', async (request, response) => {
+	const newUser: User = request.body as User;
+
+	const existingUsr = await db
+		.select()
+		.from(users)
+		.where(
+			or(eq(users.username, newUser.username), eq(users.email, newUser.email))
+		);
+
+	if (existingUsr.length !== 0) {
+		return response
+			.status(400)
+			.json({ error: 'Username or email already in use' });
+	}
+
+	const passwordHash = await bcrypt.hash(newUser.password, 10);
+
+	const addedUser = await db
+		.insert(users)
+		.values({
+			username: newUser.username,
+			fullName: newUser.fullName,
+			email: newUser.email,
+			password: passwordHash,
+		})
+		.returning();
+
+	return response.status(201).json(addedUser[0]);
 });
 
 export default router;
