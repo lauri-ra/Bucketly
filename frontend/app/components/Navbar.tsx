c1010c463d062b2115cb83da1520783760dd460f
lauri-ra <lrautiala+1@gmail.com>
2023-08-05 19:28:48 +0300

    Prettier config + tailwind formatter

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -3,23 +3,23 @@
 import Link from 'next/link';
 
 const Navbar = () => {
-	return (
-		<nav className="flex flex-wrap justify-between items-center border border-red-500">
-			<div className="ml-10 text-black text-4xl font-medium">Bucketly</div>
+  return (
+    <nav className='flex flex-wrap items-center justify-between border border-red-500'>
+      <div className='ml-10  text-4xl font-medium text-black'>Bucketly</div>
 
-			<div className="flex items-center gap-5 mr-10">
-				<Link href="/" className=" text-black text-xl font-small">
-					My buckets
-				</Link>
-				<Link href="/" className=" text-black text-xl font-small">
-					Shared buckets
-				</Link>
-				<Link href="/" className=" text-black text-xl font-small">
-					Profile
-				</Link>
-			</div>
-		</nav>
-	);
+      <div className='mr-10 flex items-center gap-5'>
+        <Link href='/' className=' font-small text-xl text-black'>
+          My buckets
+        </Link>
+        <Link href='/' className=' font-small text-xl text-black'>
+          Shared buckets
+        </Link>
+        <Link href='/' className=' font-small text-xl text-black'>
+          Profile
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
