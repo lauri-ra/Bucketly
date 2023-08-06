719821e43e685b7a9f176e474c2dc9ce418f6b34
lauri-ra <lrautiala+1@gmail.com>
2023-08-06 16:54:26 +0300

    Added basic pages/routes

---
+++ frontend/app/buckets/page.tsx
@@ -0,0 +1,3 @@
+export default async function Page() {
+	return <div>bucket list page</div>;
+}
--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -3,23 +3,37 @@
 import Link from 'next/link';
 
 const Navbar = () => {
-  return (
-    <nav className='flex flex-wrap items-center justify-between border border-red-500'>
-      <div className='ml-10  text-4xl font-medium text-black'>Bucketly</div>
+	return (
+		<nav className='flex flex-wrap items-center justify-between border border-red-500 py-5'>
+			<Link
+				href='/'
+				className='ml-10 text-4xl font-medium text-black hover:underline'
+			>
+				Bucketly
+			</Link>
 
-      <div className='mr-10 flex items-center gap-5'>
-        <Link href='/' className=' font-small text-xl text-black'>
-          My buckets
-        </Link>
-        <Link href='/' className=' font-small text-xl text-black'>
-          Shared buckets
-        </Link>
-        <Link href='/' className=' font-small text-xl text-black'>
-          Profile
-        </Link>
-      </div>
-    </nav>
-  );
+			<div className='mr-10 flex items-center gap-5'>
+				<Link
+					href='/buckets'
+					className='font-small text-base text-black hover:text-sky-500'
+				>
+					My buckets
+				</Link>
+				<Link
+					href='/buckets'
+					className=' font-small text-base text-black hover:text-sky-500'
+				>
+					Shared buckets
+				</Link>
+				<Link
+					href='/profile'
+					className=' font-small text-base text-black hover:text-sky-500'
+				>
+					Profile
+				</Link>
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
