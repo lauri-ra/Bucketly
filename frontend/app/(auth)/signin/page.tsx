e109f56d32a77d20863aeffcc58aeb1b01740cb7
lauri-ra <lrautiala+1@gmail.com>
2023-08-19 18:06:50 +0300

    Updated signin page styling

--- frontend/app/(auth)/signin/page.tsx
+++ frontend/app/(auth)/signin/page.tsx
@@ -3,6 +3,7 @@
 import { useState } from 'react';
 import { signIn } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 export default function SignIn() {
 	const [username, setUsername] = useState('');
@@ -22,11 +23,14 @@ export default function SignIn() {
 	};
 
 	return (
-		<div>
-			<form>
-				<div>
+		<div className='flex items-center justify-center p-12'>
+			<form className='flex w-1/3 flex-col items-center justify-end rounded-md border border-slate-500 px-5 py-10'>
+				<div className='text-3xl'>Log in to Bucketly</div>
+
+				<div className='mt-10'>
 					<label htmlFor='username'>Username</label>
 					<input
+						className='my-2 block w-60 rounded border bg-neutral-200 p-2 text-left text-gray-600 outline-none drop-shadow-md hover:border-lime-500 focus:border-lime-500'
 						type='text'
 						id='username'
 						value={username}
@@ -34,9 +38,11 @@ export default function SignIn() {
 						required
 					/>
 				</div>
-				<div>
+
+				<div className='mb-6'>
 					<label htmlFor='password'>Password</label>
 					<input
+						className='my-2 block w-60 rounded border bg-neutral-200 p-2 text-left text-gray-600 outline-none drop-shadow-md hover:border-lime-500 focus:border-lime-500'
 						type='password'
 						id='password'
 						value={password}
@@ -44,9 +50,20 @@ export default function SignIn() {
 						required
 					/>
 				</div>
-				<button onClick={handleSubmit} type='submit'>
+				<button
+					className='rounded  bg-lime-500 px-8 py-2'
+					onClick={handleSubmit}
+					type='submit'
+				>
 					Login
 				</button>
+
+				<div className='mt-5 py-5 font-extralight'>
+					Dont have an account?{' '}
+					<Link className='text-sky-400 hover:underline' href='/signup'>
+						Sign up
+					</Link>
+				</div>
 			</form>
 		</div>
 	);
