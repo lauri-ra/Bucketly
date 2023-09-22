5a3afe7d6adac8be28566961d633f703b0992f7c
lauri-ra <lrautiala+1@gmail.com>
2023-09-23 01:13:06 +0300

    Basic bucketlist page implementation

--- frontend/app/buckets/page.tsx
+++ frontend/app/buckets/page.tsx
@@ -1,3 +1,39 @@
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
+import { authOptions } from '../api/auth/[...nextauth]/route';
+
 export default async function Page() {
-	return <div>bucket list page</div>;
+	const session = await getServerSession(authOptions);
+
+	if (!session) {
+		redirect('/signin');
+	}
+
+	const id = session.user.id;
+	const token = session.user.token;
+
+	const response = await fetch(`${process.env.DEV_API}/buckets/user/${id}`, {
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	});
+	const buckets = await response.json();
+	console.log('bucket', buckets);
+
+	return (
+		<div>
+			bucket list page
+			<div>
+				{!buckets ? (
+					<div>Loading...</div>
+				) : (
+					buckets.map((bucket) => (
+						<div key={bucket.id}>
+							{bucket.name}: {bucket.description}
+						</div>
+					))
+				)}
+			</div>
+		</div>
+	);
 }
